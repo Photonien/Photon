e5e146b2a7cdded82925ed1c2c5c1b511fb4f3d9
Dogukan Arat <aratdogukan@gmail.com>
2022-02-20 17:01:48 +0300

    Refactor router methods

--- Service/Source/App.js
+++ Service/Source/App.js
@@ -6,8 +6,12 @@ const redis = require('redis');
 const crypto = require('crypto-js');
 const sessions = require('express-session');
 
+const app = express();
+const clientRedis = redis.createClient(6379, "redis");
+
 // Routes
 
+var RouteIndex = require("./Routes/RouteIndex");
 var RouteAuth = require("./Routes/RouteAuth");
 var RouteUsers = require("./Routes/RouteUsers");
 var RouteCollections = require("./Routes/RouteCollections");
@@ -15,22 +19,8 @@ var RouteItems = require("./Routes/RouteItems");
 
 // Globals
 
-const SecretKey = "thisismysecrctekeyfhrgfgrfrty84fwir767";
-
+const secretKey = "thisismysecrctekeyfhrgfgrfrty84fwir767";
 const appPort = process.env.PORT || 6000;
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
-app.use(sessions({
-    secret: SecretKey,
-    saveUninitialized:true,
-    resave: false
-}));
-
-const clientRedis = redis.createClient(6379, "redis");
 
 clientRedis.on("error", error => {
     console.error(error);
@@ -40,23 +30,30 @@ clientRedis.on('connect', function () {
     console.log('Backend connected to Redis server!');
 });
 
-app.get('/', function (req, res) {
-    res.send({ Message: "Welcome to API!"});
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
+app.use(sessions({
+    secret: secretKey,
+    saveUninitialized:true,
+    resave: false
+}));
+
+app.use("/", RouteIndex);
 app.use("/auth", RouteAuth);
 app.use("/users", RouteUsers);
 app.use("/collections", RouteCollections);
 app.use("/items", RouteItems);
 
 app.listen(appPort, async () => {
-    console.log(`Backend is listening to port ${appPort}`)
-    console.log(`Backend is connecting to MongoDb`);
+    console.log(`Backend is listening to port ${appPort}`);
 
     await mongoose.connect("mongodb://mongo:27017/Photon", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+    }).finally(() => {
+        console.log("Backend connected to MongoDb server!");
     });
 
-    console.log("Backend connected to MongoDb!");
 });
